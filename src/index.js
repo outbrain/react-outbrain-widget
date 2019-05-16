e50b79a66f99d8e15835d7f5573759c0efabdf41
agroisman <agroisman@outbrain.com>
2019-05-16 13:31:13 +0300

    fix params

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 
 class OutbrainWidget extends Component {
   componentDidUpdate() {
-    OBR.extern.renderSpaWidgets(this.props.permalink);
+    OBR.extern.renderSpaWidgets(this.props.dataSrc);
   }
 
   componentDidMount() {
-    OBR.extern.renderSpaWidgets(this.props.permalink);
+    OBR.extern.renderSpaWidgets(this.props.dataSrc);
   }
 
   render() {
-    const { dataSrc, dataWidgetId, obTemplate } = this.props;
+    const { dataSrc, dataWidgetId, obTemplate, obInstallationKey, obInstallationType, obAppVer, isSecured } = this.props;
     return (
       <div
         className="OUTBRAIN"
-        data-src={permalink}
-        data-widget-id={widgetId}
-        data-ob-template={templateName}
+        data-src={dataSrc}
+        data-widget-id={dataWidgetId}
+        data-ob-template={obTemplate}
         data-ob-installation-key={obInstallationKey}
         data-ob-installation-type={obInstallationType}
         data-ob-app-ver={obAppVer}
