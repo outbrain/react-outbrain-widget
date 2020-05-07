36f1d1758e59ae50c7d8d3a56294c50cd044495b
Tomer Levy <tolevy@outbrain.com>
2020-05-07 08:38:31 +0300

    switch to useEffect

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class OutbrainWidget extends Component {
-  componentDidUpdate() {
-    OBR.extern.renderSpaWidgets(this.props.dataSrc);
-  }
+const OutbrainWidget = (props) => {
+  const { dataSrc = '', dataWidgetId = '', obTemplate = '', obInstallationKey = '', obInstallationType = '', obAppVer = '', isSecured = '' } = props;
+  const { OBR = {} } = window;
 
-  componentDidMount() {
-    OBR.extern.renderSpaWidgets(this.props.dataSrc);
-  }
+  useEffect(() => {
+    if (!OBR || !OBR.extern || !OBR.extern.hasOwnProperty('renderSpaWidgets')) {
+      console.log('Missing params');
+      return;
+    }
+    OBR.extern.renderSpaWidgets(dataSrc);
+  });
 
-  render() {
-    const { dataSrc, dataWidgetId, obTemplate, obInstallationKey, obInstallationType, obAppVer, isSecured } = this.props;
-    return (
-      <div
-        className="OUTBRAIN"
-        data-src={dataSrc}
-        data-widget-id={dataWidgetId}
-        data-ob-template={obTemplate}
-        data-ob-installation-key={obInstallationKey}
-        data-ob-installation-type={obInstallationType}
-        data-ob-app-ver={obAppVer}
-        data-is-secured={isSecured}
-      />
-    );
-  }
-}
+  return (
+    <div
+      className="OUTBRAIN"
+      data-src={dataSrc}
+      data-widget-id={dataWidgetId}
+      data-ob-template={obTemplate}
+      data-ob-installation-key={obInstallationKey}
+      data-ob-installation-type={obInstallationType}
+      data-ob-app-ver={obAppVer}
+      data-is-secured={isSecured}
+    />
+  );
+};
 
 export { OutbrainWidget };
