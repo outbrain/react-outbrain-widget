779744e572eeafb7c0cf61f18eff44c0f8ba35d2
Avital Glazer <aglazer@outbrain.com>
2021-10-21 20:02:47 +0300

    add es5 version

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-//project based on https://medium.com/@BrodaNoel/how-to-create-a-react-component-and-publish-it-in-npm-668ad7d363ce
-var path = require('path');
-module.exports = {
+// project based on https://medium.com/@BrodaNoel/how-to-create-a-react-component-and-publish-it-in-npm-668ad7d363ce
+const path = require('path');
+
+module.exports = [{
   mode: 'production',
   entry: './src/index.jsx',
   output: {
@@ -21,7 +22,30 @@ module.exports = {
     ],
   },
   externals: {
-    'react': 'commonjs react',
-    'react-dom' : 'commonjs react-dom'
-  }
-};
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom',
+  },
+},
+{
+  mode: 'production',
+  entry: './src/index.jsx',
+  target: ['web', 'es5'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.es5.js',
+    libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+},
+];
